e27a98d9aefa1f62978387625356ba2076e104c7
Chris Perkins <chrisperkins99@gmail.com>
2011-10-06 17:58:10 -0400

    Forgot to inc the version number :(

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject imparsonate "0.1.0"
+(defproject imparsonate "0.2.0"
   :description "I am not a parser generator, but I play one on TV."
   :dependencies [[org.clojure/clojure "1.3.0"]]
   )
